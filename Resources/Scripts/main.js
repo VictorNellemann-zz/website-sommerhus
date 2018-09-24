cc67f0ee4cfe185e04a74170b32daee2dad26ca2
Victor Hjem <vnellemann@gmail.com>
2018-09-24 22:14:47 +0200

    Refactor calculator to a class with a constructor and add onclick

--- Resources/Scripts/main.js
+++ Resources/Scripts/main.js
@@ -10,22 +10,22 @@ Step 2:
 Refaktorisere beregneren, så den trækker på en Class "priceForStay", som består af en række properties ("firstDay", "numberOfDays", "needForCleaning",) og en function til "calculatePrice".
 */
 
-console.log("JS loaded");
+// console.log("JS loaded");
 
-week = [
-    day1 = "Mandag", 
-    day2 = "Tirsdag", 
-    day3 = "Onsdag", 
-    day4 = "Torsdag", 
-    day5 = "Fredag", 
-    day6 = "Lørdag", 
-    day7 = "Søndag"
-];
+// week = [
+//     day1 = "Mandag", 
+//     day2 = "Tirsdag", 
+//     day3 = "Onsdag", 
+//     day4 = "Torsdag", 
+//     day5 = "Fredag", 
+//     day6 = "Lørdag", 
+//     day7 = "Søndag"
+// ];
 
-var select = document.getElementById("select-first-day");
-for(index in week) {
-    select.options[select.options.length] = new Option(week[index], index);
-};
+// var select = document.getElementById("select-first-day");
+// for(index in week) {
+//     select.options[select.options.length] = new Option(week[index], index);
+// };
 
 // Array som med et for loop som fylder arrayet med tal fra 1 til 14.
 numbers = [];
@@ -33,10 +33,10 @@ for(var i = 1; i <= 14; i++){
     numbers.push(i);
 }
 
-// Et for loop som fylder formens select felt med tal fra mit numbers array
+// Et for-loop som fylder formens select felt med tal fra et numbers array, "numbers"
 var selectDays = document.getElementById("number-of-days");
 for(index in numbers) {
-    selectDays.options[select.options.length] = new Option(numbers[index], index);
+    selectDays.options[selectDays.options.length] = new Option(numbers[index], index);
 };
 
 
--- resources/Scripts/calculator.js
+++ resources/Scripts/calculator.js
@@ -1,55 +1,33 @@
-/* 
-Step 1:
+/* Step 1:
 En prisberegner som kan udregner en ca. pris for et ophold baseret på 2 parametre sendt fra en simpel form i frontenden.
 Parameter #1: Første dag: String fra en array
 Parameter #2: Antal dage: Int
 Parameter #3: Medbringer selv håndklæder og sengetøj: Ja/nej (boolean)
-Udregning: (Antal dage * X kr. per dag) + (antal dage * X kr. for strøm per dag) + gebyr for vask =
+Udregning: (Antal dage * X kr. per dag) + (antal dage * X kr. for strøm per dag) + gebyr for vask = */
 
-Step 2:
-Refaktorisere beregneren, så den trækker på en Class "priceForStay", som består af en række properties ("firstDay", "numberOfDays", "needForCleaning",) og en function til "calculatePrice".
-*/
+class PriceCalculator {
+    constructor(numberOfDays, addLaundryFee) {
+        
+        this.basePricePerDay = 250,
+        this.electricityCostPerDay = 70,
+        this.laundryFee = 100,
+        this.numberOfDays = numberOfDays,
+        this.addLaundryFee = addLaundryFee,
+        this.calculatedPrice = 0,
 
-var priceCalculator = {
-    
-    laundryFee: 100,
-    basePricePerDay: 250,
-    electricityCostPerDay: 70,
-    numberOfDays: 8,
-    // firstDayOfStay: "Mandag",
-    
-    calculatePrice: function(laundryFee, basePricePerDay, electricityCostPerDay, numberOfDays) {
-        var calculatedPrice = basePricePerDay + electricityCostPerDay * numberOfDays + laundryFee;
-        return calculatedPrice;
-    },
-    
-    get price() {
-        return this.calculatePrice();
+        this.calculate = function (basePricePerDay, electricityCostPerDay, numberOfDays, laundryFee) {
+            this.calculatedPrice = (basePricePerDay + electricityCostPerDay) * numberOfDays;
+            if (this.addLaundryFee == true) {
+                this.calculatedPrice + this.laundryFee;
+            }
+            return this.calculatedPrice;
+        }
     }
-}
+};
 
-var myPrice = new priceCalculator.price;
+function createPrice(params) {
+    var myPrice = new PriceCalculator();
+    var price = myPrice.calculate(8, true);
+    document.getElementById("price").innerHTML = price;
+};
 
-console.log(myPrice);
-
-
-/*var week = {
-    weekDays: [
-        day1 = "Mandag", 
-        day2 = "Tirsdag", 
-        day3 = "Onsdag", 
-        day4 = "Torsdag", 
-        day5 = "Fredag", 
-        day6 = "Lørdag", 
-        day7 = "Søndag"
-    ]    
-}
-
-var number = {    
-    numbers: [],
-    createListOfNumbers: function() {
-        for(var i = 1; i <= 14; i++){
-            numbers.push(i);
-        }        
-    }
-};*/
