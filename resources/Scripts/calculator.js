cffe18c337f74886ad464e4a3a96b6a90948cd7e
Victor Work <vine@dr.dk>
2018-09-29 15:12:47 +0200

    Finish up and test the calculator function

--- resources/Scripts/calculator.js
+++ resources/Scripts/calculator.js
@@ -1,33 +1,73 @@
-/* Step 1:
-En prisberegner som kan udregner en ca. pris for et ophold baseret på 2 parametre sendt fra en simpel form i frontenden.
-Parameter #1: Første dag: String fra en array
-Parameter #2: Antal dage: Int
-Parameter #3: Medbringer selv håndklæder og sengetøj: Ja/nej (boolean)
-Udregning: (Antal dage * X kr. per dag) + (antal dage * X kr. for strøm per dag) + gebyr for vask = */
 
-class PriceCalculator {
-    constructor(numberOfDays, addLaundryFee) {
-        
-        this.basePricePerDay = 250,
-        this.electricityCostPerDay = 70,
-        this.laundryFee = 100,
-        this.numberOfDays = numberOfDays,
-        this.addLaundryFee = addLaundryFee,
-        this.calculatedPrice = 0,
+// Variables on base prices declared
+// Consider loading the prices through a JSON price-list saved on the server.
 
-        this.calculate = function (basePricePerDay, electricityCostPerDay, numberOfDays, laundryFee) {
-            this.calculatedPrice = (basePricePerDay + electricityCostPerDay) * numberOfDays;
-            if (this.addLaundryFee == true) {
-                this.calculatedPrice + this.laundryFee;
-            }
-            return this.calculatedPrice;
+const basePricePerDay = 250;
+const electricityCostPerDay = 50;
+const laundryFee = 100;
+
+// Array som med et for loop som fylder arrayet 'numbers' med tal fra 1 til 14.
+
+numbers = [];
+for(var i = 0; i <= 14; i++){
+    numbers.push(i);
+}
+
+// Et for-loop som fylder formens select felt med tal fra et numbers array baseret på dets index
+var selectDays = document.getElementById("number-of-days");
+for(index in numbers) {
+    selectDays.options[selectDays.options.length] = new Option(numbers[index], index);
+};
+
+// Declaring and calling a function that loads base prices into a price table in the frontend.
+
+function listPrices(basePricePerDay, electricityCostPerDay, laundryFee) {
+    document.getElementById("base-price-per-day").innerHTML = basePricePerDay + " kr.";
+    document.getElementById("electricity-cost-per-day").innerHTML = electricityCostPerDay + " kr.";
+    document.getElementById("laundry-fee").innerHTML = laundryFee + " kr.";
+};
+
+listPrices(basePricePerDay, electricityCostPerDay, laundryFee);
+
+// Declaring the main function that does the actual calculation based on user input and our base prices
+
+function calculatePrice() {
+    let numberOfDays = 0;
+    let addLaundryFee = true;
+    let priceOfStay = 0;
+    
+    function updateNumberOfDays() {
+        numberOfDays = document.getElementById("number-of-days");
+        numberOfDays = numberOfDays.options[numberOfDays.selectedIndex].text;
+        numberOfDays = Number(numberOfDays);
+        return numberOfDays;
+    };
+    
+    function updateLaudryFee() {        
+        let selected = document.getElementById("laundry");
+        if (selected.value == "yes") {
+            addLaundryFee = true
+        } if (selected.value == "no") {
+            addLaundryFee = false
+        };        
+        return addLaundryFee;
+    };
+    
+    function updatePrice() {
+        priceOfStay = (basePricePerDay + electricityCostPerDay) * numberOfDays;
+        if (addLaundryFee == true && priceOfStay !== 0) {
+            priceOfStay = priceOfStay + laundryFee;
         }
-    }
+        return priceOfStay;
+    };
+
+    updateNumberOfDays(numberOfDays);
+    updateLaudryFee();
+    updatePrice(basePricePerDay, electricityCostPerDay, numberOfDays, laundryFee, addLaundryFee);    
+    document.getElementById("price").innerHTML = priceOfStay + " kr.";
 };
 
-function createPrice(params) {
-    var myPrice = new PriceCalculator();
-    var price = myPrice.calculate(8, true);
-    document.getElementById("price").innerHTML = price;
-};
+// Adding a click event-listener to the page.
+// The click-event calls the calculator, that returns a calculated price to a p-tag.
 
+document.getElementById("calculate").addEventListener("click", calculatePrice);
